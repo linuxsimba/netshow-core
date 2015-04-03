9c01d8a41dad876b7aa4081fe2427a9a5e02bb31
Stanley Karunditu <stanleyk@cumulusnetworks.com>
2015-04-03 22:04:20 +0000

    update docs in linux.bond.BondMember

--- netshowlib/linux/bond.py
+++ netshowlib/linux/bond.py
@@ -155,7 +155,8 @@ class BondMember(linux_iface.Iface):
     """ Linux Bond Member Attributes
 
     * **master**: pointer to :class:`Bond<netshowlib.linux.bond.Bond>` instance \
-        that this interface belongs to.
+        that this interface belongs to. This can be provided in the ``__init__`` \
+        function
     * **link_failures**: bond driver reports number of times bond member flaps
     * **bondstate**: returns whether bond member is active (1) or inactive(0) in a bond \
         **irrespective** of its carrier/linkstate status. What this means is that \
