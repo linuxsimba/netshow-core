fbe88dfa909d11c6c8ae4d86e0b3ded23a61bf46
Stanley Karunditu <stanleyk@cumulusnetworks.com>
2015-04-12 02:34:21 +0000

    update setup.py use ez_setup.py and setuptools

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
 # pylint: disable=c0111
-from distutils.core import setup
-from netshowlib import get_version
 
+from netshowlib import get_version
+import ez_setup
+ez_setup.use_setuptools()
+from setuptools import setup, find_packages
 setup(
     name='linux-netshow-lib',
     version=get_version(),
     url="http://github.com/skamithi/linux-netshow-lib",
     description="Python Library to Abstract Linux Networking Data",
     author='Stanley Karunditu',
-    author_email='stanleyk@cumulusnetworks.com',
-    packages=['netshowlib'])
+    author_email='stanleyk@linuxsimba.com',
+    package_dir={'': 'netshowlib'},
+    packages=find_packages('netshowlib'),
+    zip_safe=False,
+    license='GPLv2',
+    classifiers=[
+        'Topic :: System :: Networking',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: POSIX :: Linux'
+    ]
+)
