40d42d4e9ba003c5d3034867993ce98114f3f450
Stanley Karunditu <stanleyk@cumulusnetworks.com>
2015-04-12 13:37:18 +0000

    Revert "add python versioneer to manage code versions"
    This reverts commit 24d8420082efa51705e6f9b96542138cd9e3a6f9.

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
 # pylint: disable=c0111
-import versioneer
-versioneer.VCS = 'git'
-versioneer.versionfile_source = 'linux-netshow-lib/_version.py'
-versioneer.versionfile_build = 'linux-netshow-lib/_version.py'
-versioneer.tag_prefix = 'v'  # tags are like v1.2.0
-versioneer.parentdir_prefix = '.'
+
+from netshowlib import get_version
 import ez_setup
 ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(
     name='linux-netshow-lib',
+    version=get_version(),
     url="http://github.com/skamithi/linux-netshow-lib",
     description="Python Library to Abstract Linux Networking Data",
     author='Stanley Karunditu',
@@ -22,7 +19,5 @@ setup(
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
         'Operating System :: POSIX :: Linux'
-    ],
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
+    ]
 )
