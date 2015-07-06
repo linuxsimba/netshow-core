e65355642fa59a81f508b80bfe1f1fd7fefb5c77
Stanley Karunditu <stanleyk@cumulusnetworks.com>
2015-07-06 20:02:01 +0000

    add upload scripts

---
+++ netshow-lib/upload.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+# Switch to the correct directory
+if [ ! -f setup.py ]; then
+  cd `dirname $0`
+fi
+
+set -e
+
+echo "starting up"
+GIT_BRANCH=test
+PATH=$WORKSPACE/venv/bin:/usr/local/bin:$PATH
+if [ ! -d "venv" ]; then
+        virtualenv venv
+fi
+. venv/bin/activate
+
+# install test requirements like tox
+pip install --upgrade -r requirements_develop.txt
+python setup.py  bdist_wheel sdist upload -r testing
