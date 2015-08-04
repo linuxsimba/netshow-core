b9473e73f868a5a8deeeba6819e0cdac0d33db19
Stanley Karunditu <stanleyk@cumulusnetworks.com>
2015-08-04 05:15:27 +0000

    set long description to point to the readme

--- netshow/setup.py
+++ netshow/setup.py
@@ -5,6 +5,9 @@ try:
 except ImportError:
     pass
 
+with open('README') as f:
+    readme_content = f.read().strip()
+
 from _gitversion import get_version
 from setuptools import setup, find_packages
 setup(
@@ -12,6 +15,7 @@ setup(
     version=get_version(),
     url="http://github.com/CumulusNetworks/netshow",
     description="Linux Network Troubleshooting Tool",
+    long_description=readme_content,
     author='Cumulus Networks',
     author_email='ce-ceng@cumulusnetworks.com',
     packages=find_packages(),
